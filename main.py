cc09eaf043d94d0715ecf2799296e05f9cfb6149
Женя <golovatow@mail.ru>
2022-08-15 13:48:48 +0300

    STR

---
+++ main.py
@@ -0,0 +1,22 @@
+#Способы обозначения строк в кавычки:
+print('Lesson')
+print("Lesson")
+print("""Lesson""")
+print('''Lesson''')
+print("Ресторан 'Marletto' ")
+print('Ресторан "Marletto" ')
+print("Ресторан \"Marletto\" ")
+
+#Сложение и умножение строк:
+name = " Hello " + " World "
+print(name)
+print(" Hello "* 100)
+
+#Преобразование типов данных в строку:
+number = "58"
+print(int(number))
+number1 = "58.03"
+print(float(number1))
+name1 = "Женя"
+age = 25
+print("person: " + name1 + ", age: " + str(age))
